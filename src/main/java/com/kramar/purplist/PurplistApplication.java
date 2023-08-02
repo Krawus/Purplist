7938127d0cd5b84469523fcadda81c565bfa006a
Krawus <krawczykadam21@gmail.com>
2023-08-02 20:49:51 +0200

    init

---
+++ src/main/java/com/kramar/purplist/PurplistApplication.java
@@ -0,0 +1,13 @@
+package com.kramar.purplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurplistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurplistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kramar/purplist/PurplistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kramar.purplist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurplistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
