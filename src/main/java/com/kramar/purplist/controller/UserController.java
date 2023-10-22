ec21c4e09e8706b133bc1c478c34ab952e7222f7
Krawus <krawczykadam21@gmail.com>
2023-10-22 16:33:57 +0200

    updating user lists

--- src/main/java/com/kramar/purplist/controller/UserController.java
+++ src/main/java/com/kramar/purplist/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.kramar.purplist.controller;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +47,27 @@ public class UserController {
         return loggedUser.getPurplists();
 
     }
+    
+    @GetMapping("/lists/{listIndex}")
+    public Purplist getUserList(Principal principal, @PathVariable int listIndex){
+        String loggedUsername = principal.getName();
+        User loggedUser = userService.findByUsername(loggedUsername);
+        List<Purplist> userLists = loggedUser.getPurplists();
+
+        return userLists.get(listIndex);
+
+    }
+
+    @PutMapping("/lists/{listIndex}")
+    public Purplist putUserPurplist(@RequestBody Purplist purplistDetails, @PathVariable int listIndex, Principal principal){
+
+        String loggedUsername = principal.getName();
+        User loggedUser = userService.findByUsername(loggedUsername);
+
+        Purplist listToUpdate = loggedUser.getPurplists().get(listIndex);
+        
+        return purplistService.update(purplistDetails, listToUpdate.getId());
+    }
+    
 
 }
