5472d3411a0d2e708e9f95e3afa8e8fca284fc7f
Krawus <krawczykadam21@gmail.com>
2024-12-29 20:37:29 +0100

    added invitation table creation script to main script

--- sql_scripts/create_database_for_purplist.sql
+++ sql_scripts/create_database_for_purplist.sql
@@ -66,4 +66,18 @@ INSERT INTO authorities (username, authority)
 -- --insert_values_into_list.sql -> insert values into the lists table
 INSERT INTO lists(name, content) values ('test_list2', '{"element1": 1, "element2": 0}');
 INSERT INTO lists(name, content) values ('shopping_list', '{"ketchup": 1, "garlic": 0, "water":0}');
+
+-- create_invitation_table.sql -> create the invitations table
+DROP TABLE IF EXISTS invitations;
+CREATE TABLE invitations(
+        id int NOT NULL auto_increment,
+        purplist_id int NOT NULL,
+        sender_id int NOT NULL,
+        receiver_id int NOT NULL,
+        accepted TINYINT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (purplist_id) REFERENCES lists(id),
+        FOREIGN KEY (sender_id) REFERENCES users(id),
+        FOREIGN KEY (receiver_id) REFERENCES users(id)
+    );
     
